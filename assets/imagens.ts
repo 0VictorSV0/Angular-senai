1585be66e9fb7ee0b09fd4320bb18d50a6f7b298
Victor <victorsegurvieira@gmail.com>
2022-12-13 18:25:43 -0300

    atualização1

---
+++ assets/imagens.ts
@@ -0,0 +1,32 @@
+export let img = [
+    {
+        "code": 1,
+        "name": "controles-fundoazul",
+        "image": "https://cdn.pixabay.com/photo/2021/09/07/07/11/game-console-6603120_960_720.jpg"
+    },
+    {
+        "code": 2,
+        "name": "controle",
+        "image": "https://cdn.pixabay.com/photo/2017/08/10/06/06/controller-2618897_960_720.jpg"
+    },
+    {
+        "code": 3,
+        "name": "ps4",
+        "image": "https://cdn.pixabay.com/photo/2017/05/19/14/09/ps4-2326616_960_720.jpg"
+    },
+    {
+        "code": 4,
+        "name": "headphone",
+        "image": "https://cdn.pixabay.com/photo/2016/05/07/07/39/headphones-1377194_960_720.jpg"
+    },
+    {
+        "code": 5,
+        "name": "controle-2",
+        "image": "https://cdn.pixabay.com/photo/2016/02/23/02/17/joystick-1216816_960_720.jpg"
+    },
+    {
+        "code": 6,
+        "name": "controle-3",
+        "image": "https://cdn.pixabay.com/photo/2017/06/29/10/28/games-2453777_960_720.jpg"
+    }
+]
